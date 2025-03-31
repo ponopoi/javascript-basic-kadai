138ac96d8c6081be98305e98ec54697683d316c1
ponopoi <CrIqipR7H3gl@outlook.jp>
2025-03-31 09:12:29 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+const currentDate = new Date();
+
+const year =currentDate.getFullYear();
+
+const month = currentDate.getMonth() + 1;
+
+const day = currentDate.getDate();
+
+console.log(` ${year}年${month}月${day}日`);
+
+
+
+
